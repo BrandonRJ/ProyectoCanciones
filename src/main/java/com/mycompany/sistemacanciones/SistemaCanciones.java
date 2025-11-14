2d257f834aa3c391f844cd2e464100c29d17c019
BrandonRJ <Brandon-95cr@hotmail.com>
2025-11-14 00:18:56 -0600

    Mi primer Commit

---
+++ src/main/java/com/mycompany/sistemacanciones/SistemaCanciones.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemacanciones;
+
+/**
+ *
+ * @author Brandon
+ */
+public class SistemaCanciones {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
